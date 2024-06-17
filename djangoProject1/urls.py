55748291c57f38e39f0858d14093925cea609ffe
VladPitko <92929619+VladPitko@users.noreply.github.com>
2024-06-17 03:49:49 +0200

    register login

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="base.html"), name="base"),
+    path("register/", views.CreateUserView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login")
 ]
